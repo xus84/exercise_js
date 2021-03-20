17bc8343f46eb08b51a27bf7eefb161fde687ce8
garsan84 <jgrsan84@gmail.com>
2021-03-20 19:30:04 +0100

    commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const message = document.getElementById('message').innerHTML = "hello";
+console.log(message);
+/*
+const listItems = document.getElementsByTagName('li');
+console.log(listItems);
+
+const highlights = document.getElementsByClassName('highlight');
+console.log(highlights);
+
+const val1 = document.querySelector('li');
+console.log(val1);
+
+const val2 = document.querySelector('#message');
+console.log(val2);
+
+const val3 = document.querySelector('.highlight');
+console.log(val3);*/
+
